4586893641bf7b40e91556da7a7ab09a8794fac5
오은지 <81677536+oheunji05@users.noreply.github.com>
2021-11-07 00:55:48 +0900

    [ADD] file

---
+++ 문제 풀이/src/Int.java
@@ -0,0 +1,10 @@
+public class Int {
+    //세 개의 정수형 변수를 선언하고 각 변수에 10 20 30을 대입한 후 그 변수를 이용하여 출력 예와 같이 출력하는 프로그램을 작성하시오.
+    //
+    //출력 예
+    //10 + 20 = 30
+    public static void main(String[] args) {
+        int a=100,b=20,c=30;
+        System.out.print(a+" + "+b+" = "+c);
+    }
+}
